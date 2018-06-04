c972578ccfa727f13f07e780e5e87e9fb70726ed
Karl Huber <Karl Huber@KarlsPC.fritz.box>
2018-06-04 18:00:25 +0200

    Ability update n shit

---
+++ src/de/sag/mazehunter/game/player/abilities/FireballListener.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.sag.mazehunter.game.player.abilities;
+
+import com.esotericsoftware.kryonet.Connection;
+import de.sag.mazehunter.Main;
+import de.sag.mazehunter.game.Config;
+import de.sag.mazehunter.game.player.InputListener;
+import de.sag.mazehunter.server.networkData.abilities.DashRequest;
+import de.sag.mazehunter.server.networkData.abilities.DashResponse;
+import de.sag.mazehunter.utils.Vector2;
+
+/**
+ *
+ * @author Karl Huber
+ */
+public class FireballListener extends InputListener{
+
+    @Override
+    public void received(Connection connection, Object object) {
+        if(object instanceof DashRequest) {
+        }
+    }
+        
+    public void SendFireballResponse(int id) {
+        Main.MAIN_SINGLETON.server.sendToAllUDP(null);
+    }
+}

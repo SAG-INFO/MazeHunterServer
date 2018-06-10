592fadb34b75067ca846755b818b57f74d14cf6e
Samuel Reiser <sreis@DESKTOP-14UOVNH.fritz.box>
2018-06-10 21:55:56 +0200

    Collision!

--- src/de/sag/mazehunter/game/player/Player.java
+++ src/de/sag/mazehunter/game/player/Player.java
@@ -18,21 +18,21 @@ import de.sag.mazehunter.utils.Vector2;
  */
 public class Player {
 
-    public final Vector2 position;
-    public final Vector2 velocity;
-    public int connectionID;
+    private final float size = 6;
+    public final Vector2 position = new Vector2(35, 35);
+    public final Vector2 desiredVelocity = new Vector2(0, 0);
+    public final Vector2 velocity = new Vector2(0, 0);
     public float speed;
     public float movementSpeedFactor;
+    
+    public int connectionID;
+    
     public int maxHealth;
     public int currentHealth;
 
     private final Vector2 tmp = new Vector2();
 
     public Player(int id) {
-        position = new Vector2();
-        position.set(30f, 30f);
-        velocity = new Vector2();
-        velocity.set(0f, 0f);
         connectionID = id;
         speed = Config.DEFAULT_SPEED;
         movementSpeedFactor = 1.0f;
@@ -74,8 +74,24 @@ public class Player {
         calcCD2();
         this.position.add(tmp.set(velocity).scl(delta));
     }
-
-    //    public void calcCD() {
+    
+    public void calcCD2() {
+        int signX = Integer.signum((int)velocity.x);
+        int signY = Integer.signum((int)velocity.y);
+        if(collides(tmp.set(position).add(size/2, size*signY)) || collides(tmp.set(position).add(-size/2, size*signY))){
+            velocity.y = 0;
+            InputListener.sendMovementResponse(this);
+        }if(collides(tmp.set(position).add(size*signX, size/2)) || collides(tmp.set(position).add(size*signX, -size/2))){
+            velocity.x = 0;
+            InputListener.sendMovementResponse(this);
+        }
+    }
+    
+    private boolean collides(Vector2 position){
+        return !MAIN_SINGLETON.game.world.talktoTile(position).open;
+    }
+    
+//    public void calcCD() {
 //        if (velocity.y > 0 && !MAIN_SINGLETON.game.world.talktoTile(tmp.set(position).add(0, 1)).open) {
 //            velocity.y = 0;
 //            InputListener.sendMovementResponse(this);
@@ -90,11 +106,11 @@ public class Player {
 //            InputListener.sendMovementResponse(this);
 //        }
 //    }
-    public void calcCD2() {
-        if (!MAIN_SINGLETON.game.world.talktoTile(tmp.set(position).add(0, Integer.signum((int) velocity.y))).open) {
-            velocity.y = 0;
-        } else if (!MAIN_SINGLETON.game.world.talktoTile(tmp.set(position).add(Integer.signum((int) velocity.x), 0)).open) {
-            velocity.x = 0;
-        }
-    }
+//    public void calcCD2() {
+//        if (!MAIN_SINGLETON.game.world.talktoTile(tmp.set(position).add(0, Integer.signum((int) velocity.y))).open) {
+//            velocity.y = 0;
+//        } else if (!MAIN_SINGLETON.game.world.talktoTile(tmp.set(position).add(Integer.signum((int) velocity.x), 0)).open) {
+//            velocity.x = 0;
+//        }
+//    }
 }
